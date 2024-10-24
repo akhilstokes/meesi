2bfcc97e42551da5bb833c2515573e8733fc7cf8
akhil <akhilnk@gmail.com>
2024-10-24 11:08:21 +0530

    eigth commit

--- Untidata .c
+++ Untidata .c
@@ -6,20 +6,7 @@ int x,b,n=10;
 for (i=0; i<10 ;i++)
      a[i]=i+1 ;
 for (i = 0; i < n; i++)
-     printf("%d ", a[i]);
-     printf("\n");
- x=50;
- b=5;
- n++;
- for  (i=n-1;i>=b; i--)
-     a[i]=i-1;
- a[b-1]=x;
-
-for (i = 0; i < n; i++)
-        printf("%d ", a[i]);
-    printf("\n");
-    return 0;
-}
+     printf("%d ",gfffffffff
 
 
 
