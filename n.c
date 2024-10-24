c2bb9d132d86a7cf4e02d08c5851a9b52d3bf2f3
akhil <akhilnk@gmail.com>
2024-10-24 11:03:08 +0530

    sixth commit

--- n.c
+++ n.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    int num, originalNum, remainder, result = 0;
+    int num, origin
     printf("Enter a three-digit integer: ");
     scanf("%d", &num);
     originalNum = num;
